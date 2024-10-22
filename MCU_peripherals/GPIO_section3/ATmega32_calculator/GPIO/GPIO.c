8e741756ff7a5a330aa26308586c7d8c3e9a8c9b
HazemShaaban101 <7azemsh3baan@gmail.com>
2024-10-22 14:25:53 +0300

    this time i used the same calculator app from the BLUEPILL project to showcase the layered architecture. the app layer is unmodified, the HAL layer is slightly modified for different pin configurations, and the MCAL layer consists of the GPIO drivers i made for the ATmega32. both the stm32 and the atmega drivers have the same names and functionality for their APIs, which allowed us to use the same HAL layer across the two MCUs with only some slight modifications. the drivers can be found in the drivers folder in the repo. work on the drivers is incremental so always check the documentation for the available APIs

---
+++ MCU_peripherals/GPIO_section3/ATmega32_calculator/GPIO/GPIO.c
@@ -0,0 +1,145 @@
+/************************************************************************************************
+				This file is the GPIO drivers for ATmega32 devices
+				this file include:
+					
+					
+				this file is incremental, meaning it is constantly updated based on
+				my next project requirements. it is not fully functional for all GPIO
+				functions until specified otherwise.
+				
+				
+				*************************************************************
+				*				Made by: Hazem Shaaban Bakry				*
+				*				github: HazemShaaban101						*
+				*				Linkedin: hazemshaaban101					*
+				*************************************************************
+*************************************************************************************************/
+
+//===============================================================================================
+//	important includes
+//===============================================================================================
+
+#include "../Inc/GPIO.h"
+
+//===============================================================================================
+//	Function definitions
+//===============================================================================================
+
+
+/**===================================================================================================================================================
+* @Fn				- GPIO_Pin_Init
+* @brief 			- initializes a selected pin based on selected port and pin number and pin configurations
+* @param [in] 		- GPIOx: pointer to the desired GPIO port, the pointer is casted to GPIO_t which is a structure that contains all GPIO registers
+* @param [in] 		- PinNumber: the number of desired pin on the port
+* @param [in] 		- PinMode: the desired operation mode for the selected pin
+* @retval 			- none
+* Note				- none
+*/
+void GPIO_Pin_Init(volatile GPIO_t * GPIOx, uint8_t PinNumber, uint8_t PinMode)
+{
+	switch (PinMode)
+	{
+		case GPIO_MODE_IN_FLOATING:
+			GPIOx->DDR &= ~(1 << PinNumber);
+			GPIOx->PORT &= ~(1 << PinNumber);
+			break;
+		case GPIO_MODE_IN_PU:
+			GPIOx->DDR &= ~(1 << PinNumber);
+			GPIOx->PORT |= (1 << PinNumber);
+		case GPIO_MODE_OUT_PP:
+			GPIOx->DDR |= (1 << PinNumber);
+	}
+}
+
+/**===================================================================================================================================================
+* @Fn				- GPIO_Port_DeInit
+* @brief 			- Resets a selected port
+* @param [in] 		- GPIOx: pointer to the desired GPIO port, the pointer is casted to GPIO_t which is a structure that contains all GPIO registers
+* @retval 			- none
+* Note				- none
+*/
+void GPIO_Port_DeInit(volatile GPIO_t * GPIOx)
+{
+	GPIOx->DDR = 0;
+	GPIOx->PORT = 0;
+}
+
+
+/**===================================================================================================================================================
+* @Fn				- GPIO_Pin_Read
+* @brief 			- Reads a selected pin on a selected port
+* @param [in] 		- GPIOx: pointer to the desired GPIO port, the pointer is casted to GPIO_t which is a structure that contains all GPIO registers
+* @param [in] 		- PinNumber: number of pin user wants to read from
+* @retval 			- Pin reading based on values from GPIO-h:@ref GPIO_PIN_VALUE
+* Note				- none
+*/
+uint8_t GPIO_Pin_Read(volatile GPIO_t * GPIOx, uint8_t PinNumber)
+{
+	return ((GPIOx->PIN & (1 << PinNumber)) >> PinNumber);
+}
+
+
+/**===================================================================================================================================================
+* @Fn				- GPIO_Port_Read
+* @brief 			- Reads a selected port
+* @param [in] 		- GPIOx: pointer to the desired GPIO port, the pointer is casted to GPIO_t which is a structure that contains all GPIO registers
+* @retval 			- the values on all pins of port in the form of an unsigned 8 bit integer
+* Note				- none
+*/
+uint8_t GPIO_Port_Read(volatile GPIO_t * GPIOx)
+{
+	return GPIOx->PIN;
+}
+
+
+/**===================================================================================================================================================
+* @Fn				- GPIO_Pin_Write
+* @brief 			- writes to a selected pin in a selected port
+* @param [in] 		- GPIOx: pointer to the desired GPIO port, the pointer is casted to GPIO_t which is a structure that contains all GPIO registers
+* @param [in] 		- PinNumber: the number of pin user wants to write to
+* @param [in] 		- value: the value user wants to write to pin
+* @retval 			- none
+* Note				- none
+*/
+void GPIO_Pin_Write(volatile GPIO_t * GPIOx, uint8_t PinNumber, uint8_t value)
+{
+	if (value == GPIO_LOW)
+	{
+		GPIOx->PORT &= ~(1 << PinNumber);
+	}
+	else
+	{
+		GPIOx->PORT |= 1 << PinNumber;
+	}
+}
+
+
+/**===================================================================================================================================================
+* @Fn				- GPIO_Port_Write
+* @brief 			- writes to a selected port
+* @param [in] 		- GPIOx: pointer to the desired GPIO port, the pointer is casted to GPIO_t which is a structure that contains all GPIO registers
+* @param [in] 		- value: the value user wants to write to Port
+* @retval 			- none
+* Note				- none
+*/
+void GPIO_Port_Write(volatile GPIO_t * GPIOx, uint8_t value)
+{
+	GPIOx->PORT = value;
+}
+
+
+/**===================================================================================================================================================
+* @Fn				- GPIO_Pin_Toggle
+* @brief 			- toggles a selected pin on a selected port
+* @param [in] 		- GPIOx: pointer to the desired GPIO port, the pointer is casted to GPIO_t which is a structure that contains all GPIO registers
+* @param [in] 		- PinNumber: the pin user wants to toggle
+* @retval 			- none
+* Note				- none
+*/
+void GPIO_Pin_Toggle(volatile GPIO_t * GPIOx, uint8_t PinNumber)
+{
+	GPIOx->PORT ^= 1 << PinNumber;
+}
+
+
+
---
+++ MCU_peripherals/GPIO_section3/ATmega32_calculator/numpad_drivers/numpad.c
@@ -0,0 +1,123 @@
+/************************************************************************************************
+				This file is the Numpad drivers
+				this file include:
+					
+					
+				this file is incremental, meaning it is constantly updated based on
+				my next project requirements. it is not fully functional for all GPIO
+				functions until specified otherwise.
+				
+				
+				*************************************************************
+				*				Made by: Hazem Shaaban Bakry				*
+				*				github: HazemShaaban101						*
+				*				Linkedin: hazemshaaban101					*
+				*************************************************************
+*************************************************************************************************/
+
+
+
+//===============================================================================================
+//	important includes
+//===============================================================================================
+#include "../Inc/numpad.h"
+
+
+
+//===============================================================================================
+//	Global variables
+//===============================================================================================
+static char pad_matrix[4][4] = {{'7','8','9','/'},{'4','5','6','*'},{'1','2','3','-'},{'?','0','=','+'}};
+
+
+//===============================================================================================
+//	Function definitions
+//===============================================================================================
+
+
+
+/**===================================================================================================================================================
+* @Fn				- DELAY
+* @brief 			- does a delay by trapping the processor in a loop
+* @param [in] 		- x: the number of loops desired, each loop counts from 0 to 255
+* @retval 			- none
+* Note				- not the best approach, should be replaced with MCU's dedicated delay function
+*/
+
+static void DELAY(unsigned int x)
+{
+	int i, j;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < 255; j++);
+	}
+}
+
+
+/**===================================================================================================================================================
+* @Fn				- NUMPAD_init
+* @brief 			- Initializes pins used by the numpad
+* @param [in] 		- none
+* @retval 			- none
+* Note				- none
+*/
+
+void NUMPAD_init(void)
+{
+	// first 4 pins output, last 4 pins input pull-up
+	// NUMPAD_PORT_DIR	= 0x0F;
+	GPIO_Pin_Init(NUMPAD_PORT, 0, GPIO_MODE_OUT_PP);
+	GPIO_Pin_Init(NUMPAD_PORT, 1, GPIO_MODE_OUT_PP);
+	GPIO_Pin_Init(NUMPAD_PORT, 2, GPIO_MODE_OUT_PP);
+	GPIO_Pin_Init(NUMPAD_PORT, 3, GPIO_MODE_OUT_PP);
+	
+	GPIO_Pin_Init(NUMPAD_PORT, 4, GPIO_MODE_IN_FLOATING);
+	GPIO_Pin_Init(NUMPAD_PORT, 5, GPIO_MODE_IN_FLOATING);
+	GPIO_Pin_Init(NUMPAD_PORT, 6, GPIO_MODE_IN_FLOATING);
+	GPIO_Pin_Init(NUMPAD_PORT, 7, GPIO_MODE_IN_FLOATING);
+	// output pins high, input pins pulled high
+	// NUMPAD_WRITE = 0xFF;
+	GPIO_Pin_Write(NUMPAD_PORT, 0, GPIO_HIGH);
+	GPIO_Pin_Write(NUMPAD_PORT, 1, GPIO_HIGH);
+	GPIO_Pin_Write(NUMPAD_PORT, 2, GPIO_HIGH);
+	GPIO_Pin_Write(NUMPAD_PORT, 3, GPIO_HIGH);
+
+}
+
+
+
+
+/**===================================================================================================================================================
+* @Fn				- NUMPAD_read
+* @brief 			- Reads the pressed button on the numpad
+* @param [in] 		- none
+* @retval 			- returns a char based on pad_matrix array
+* Note				- none
+*/
+
+char NUMPAD_read(void)
+{
+	int i, j;
+	for (i = 0; i < 4; i++)
+	{
+		// turn off pin i
+		// NUMPAD_WRITE &= ~(1 << i);
+		GPIO_Pin_Write(NUMPAD_PORT, i, GPIO_LOW);
+		
+		for (j = 4; j < 8; j++)
+		{
+			if (GPIO_Pin_Read(NUMPAD_PORT, j) == 0)
+			{
+				while (GPIO_Pin_Read(NUMPAD_PORT, j) == 0);		// stay in loop until button is not pressed
+				
+				GPIO_Pin_Write(NUMPAD_PORT, i, GPIO_HIGH);
+				return pad_matrix[i][j - 4];
+			}
+			DELAY(20);	
+		}
+		// NUMPAD_WRITE |= 1<<i;
+		GPIO_Pin_Write(NUMPAD_PORT, i, GPIO_HIGH);
+		DELAY(20);
+	}
+	return 0;
+}
